f9a80f1fa70d2d1f3bc67ef826a0f423d9dcd9b4
ryom0624 <37314838+ryom0624@users.noreply.github.com>
2023-10-30 09:52:12 +0900

    fix typo

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { lustitana } from "@/app/ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 import Search from "../search";
 import { CustomersTable, FormattedCustomersTable } from "@/app/lib/definitions";
 
 export default async function CustomersTable({ customers }: { customers: FormattedCustomersTable[] }) {
   return (
     <div className="w-full">
-      <h1 className={`${lustitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
       <Search placeholder="Search customers..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
