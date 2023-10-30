9159f053531552110d070ebca39555b1a01ac6b1
ryom0624 <37314838+ryom0624@users.noreply.github.com>
2023-10-30 09:39:01 +0900

    done chapter5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lustitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
